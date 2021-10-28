077c625ff380a45b1098f60527d2aa2c07ddf416
Sergey Kosarevsky <sk@linderdaum.com>
2021-10-28 18:21:32 +0300

    Initialization/deinitialization + basic logging

---
+++ minilog.cpp
@@ -0,0 +1,137 @@
+﻿/**
+minilog
+
+MIT License
+
+Copyright (c) 2021 Sergey Kosarevsky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+**/
+
+#include "minilog.h"
+
+#include <stdarg.h>
+#include <time.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+#	define WIN32_LEAN_AND_MEAN
+#	define NOMINMAX
+#	define NOIME
+#	define WIN32_SUP
+#	include <windows.h>
+#else
+#	include <sys/time.h>
+#endif
+
+namespace {
+	minilog::LogConfig config = {};
+	FILE* logFile = nullptr;
+} // namespace
+
+bool minilog::initialize(const minilog::LogConfig& cfg)
+{
+	if (logFile)
+		deinitialize();
+
+	logFile = fopen(cfg.fileName, "w");
+
+	if (!logFile)
+		return false;
+
+	config = cfg;
+
+	if (config.writeIntro)
+	{
+		log(minilog::Log, "minilog: initializing ...");
+		log(minilog::Log, "minilog: log file: %s", cfg.fileName);
+	}
+
+	return true;
+}
+
+void minilog::deinitialize()
+{
+	if (!logFile)
+		return;
+
+	if (config.writeOutro)
+		log(minilog::Log, "minilog: deinitializing ...");
+
+	fflush(logFile);
+	fclose(logFile);
+
+	logFile = nullptr;
+}
+
+static uint32_t getCurrentMilliseconds()
+{
+#if defined(_WIN32) || defined(_WIN64)
+	SYSTEMTIME st;
+	GetSystemTime(&st);
+	return st.wMilliseconds;
+#else
+	struct timeval timeVal;
+	gettimeofday(&timeVal, nullptr);
+	return timeVal.tv_usec / 1000;
+#endif
+}
+
+static uint32_t writeTimeStamp(char* buffer, uint32_t maxLength)
+{
+	time_t tempTime;
+	time(&tempTime);
+	::tm tmTime = *localtime(&tempTime);
+
+	const int n = snprintf(buffer, maxLength, "%02d:%02d:%02d.%03d   ", tmTime.tm_hour, tmTime.tm_min, tmTime.tm_sec, getCurrentMilliseconds());
+
+	return n > 0 ? n : 0;
+}
+
+static void writeMessageToLog(const char* msg)
+{
+	if (!logFile)
+		return;
+
+	fprintf(logFile, "%s\n", msg);
+
+	if (config.forceFlush)
+		fflush(logFile);
+}
+
+void minilog::log(eLogLevel level, const char* format, ...)
+{
+	if (level < config.logLevel)
+		return;
+
+	constexpr uint32_t kBufferLength = 8192;
+
+	char buffer[kBufferLength];
+
+	const uint32_t tsLength = writeTimeStamp(buffer, kBufferLength);
+
+	if (tsLength >= kBufferLength)
+		return;
+
+	va_list args;
+	va_start(args, format);
+	vsnprintf(buffer + tsLength, kBufferLength - tsLength, format, args);
+	va_end(args);
+
+	writeMessageToLog(buffer);
+}
---
+++ minilog.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+/**
+minilog
+
+MIT License
+
+Copyright (c) 2021 Sergey Kosarevsky
+**/
+
+namespace minilog
+{
+	enum eLogLevel
+	{
+		Paranoid 	= 0,
+		Debug			= 1,
+		Log			= 2,
+		Warning		= 3,
+		FatalError	= 4
+	};
+
+	struct LogConfig
+	{
+		const char* fileName = "";
+		eLogLevel logLevel = minilog::Log;
+		bool forceFlush = true;
+		bool writeIntro = true;
+		bool writeOutro = true;
+	};
+
+	bool initialize(const LogConfig& cfg);
+	void deinitialize();
+	void log(eLogLevel level, const char* format, ...);
+} // namespace minilog
